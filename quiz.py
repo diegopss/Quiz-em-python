49e94e643cd85da79232c95eeea76056b6660f78
Diego passos <die-gops@hotmail.com>
2022-05-14 16:38:44 -0300

    erro ao cadastrar jogadores no Json, corrigido

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ import random
 #Esta função pega o arquivo JSON e retorna uma lista com as questoes
 #OBS: AS VEZES O PYTHON NÃO RECONHECE O CAMINHO RELATIVO. (resolvi colocando o caminho absoluto)
 def lista_questoes():
-    with open("/Users/die-g/POJETOS/Quiz-em-python/perguntas.json", encoding="utf8") as arquivo:
+    with open("C:/Users/die-g/PROJETOS/Quiz-em-python/perguntas.json", encoding="utf8") as arquivo:
 
         json_perguntas = json.load(arquivo)
     
@@ -19,26 +19,26 @@ def questao(id):
 #esta função retorna uma lista com os jogadores 
 #OBS: AS VEZES O PYTHON NÃO RECONHECE O CAMINHO RELATIVO. (resolvi colocando o caminho absoluto)
 def lista_jogadores():
-     with open("/Users/die-g/POJETOS/Quiz-em-python/jogadores.json","r") as arquivo:
+     with open("C:/Users/die-g/PROJETOS/Quiz-em-python/jogadores.json","r") as arquivo:
         jogador = json.load(arquivo)
-        jogador = jogador["jogador"]
+        
         return jogador
 #esta função escreve os dados do jogador no Json
 #OBS: AS VEZES O PYTHON NÃO RECONHECE O CAMINHO RELATIVO. (resolvi colocando o caminho absoluto)
 def armazenar_jogador(nome,acertos):
     jogador = lista_jogadores()
+    
     jogador["jogador"] += [{"nome": nome,"acertos":acertos}]
-    with open("/Users/die-g/POJETOS/Quiz-em-python/jogadores.json","w") as arquivo:        
+    with open("C:/Users/die-g/PROJETOS/Quiz-em-python/jogadores.json","w") as arquivo:        
         json.dump(jogador, arquivo, indent=4)
 
 #Função que exibe um ranking dos jogadores(ainda não consegui fazer corretamente T_T )
 def ranking():
     jogadores = lista_jogadores()   
-    for jogador in jogadores:
+    for jogador in jogadores["jogador"]:
         if jogador["acertos"] == 10:
             print("-------------------------------Jogadores nota 10-------------------------------")
             print(f"{jogador['nome']}  Nota: {jogador['acertos']} 🌟")
-ranking()   
 
 def quiz():
     acertos = 0
@@ -54,14 +54,17 @@ def quiz():
         print(f"3){questao_random['alt3']}")
         print(f"4){questao_random['alt4']}")
         print(f"5){questao_random['alt5']}\n")
-
+        print("----------------------------------------------------------------------------")
         resp = input("Digite a alternativa correta: ")
         if resp == questao_random["correct_alt"]:
             acertos += 1
-    
+            print(f"Resposta correta ✅")
+        else: print(f"Resposta incorreta ❌ (A alternativa correta era {questao_random['correct_alt']})")
+    print("----------------------------------------------------------------------------")
     nome = input("Qual o seu nome? ")
     print(f"Parabéns {nome} você acertou {acertos} questões")
     armazenar_jogador(nome, acertos)    
+    print("----------------------------------------------------------------------------")
     ranking()
 
 
